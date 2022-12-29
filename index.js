9a19450c4cb29f79087c3b1d802c7463fae82878
Bernard Yamoah <bernardyamoah2016@gmail.com>
2022-12-29 00:14:20 +0000

    finished counter app

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const increment_btn= document.getElementById('increament-btn');
 
 increment_btn.addEventListener('click',()=>{
     score ++;
-    count.innerText=score;
+    count.textContent=score;
     console.log(count);
 })
  function decreament() {
     score--;
-    count.innerText=score;
+    count.textContent=score;
     console.log(count);
  }
  function save() {
 let countStr = score + " - "
 peopleNumber.textContent += countStr
+count.textContent= 0
+score=0
+}
+function clearCount(){
+    score=0;
+    count.textContent="0";
+    
+    peopleNumber.textContent="";
 }
