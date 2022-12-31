ff31bcd966121ef3bb3833ae949665e171789c84
Bernard Yamoah <bernardyamoah2016@gmail.com>
2022-12-31 00:50:05 +0000

    update

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const displayP =document.getElementById('display');
-let firstCard = 14
+let firstCard = 12
 let secondCard = 7
-
+let hasBlackJack=false
+let isAlive=true
 let sum = firstCard + secondCard
-if(sum<=20){
-    displayP.innerText="Do you want to draw a new card? 🙂"
+let message=""
+let messageEl= document.getElementById('message-el')
+let cardEl=document.getElementById('card-el')
+let sumEl=document.querySelector('#sum-el')
+let cards=[firstCard,secondCard] //array - ordered list of items 
+
+function startGame(){
+    renderGame()
 }
-else if(sum===21){
-    displayP.innerText="Wohoo! You've got Blackjack! 😎"
+
+function renderGame() {
+	sumEl.textContent='Sum: '+ sum;
+cardEl.textContent='Cards:'+ cards[0] + " & " + cards[1]
+	if(sum<=20){
+	    message="Do you want to draw a new card? 🙂"
+	}
+	else if(sum===21){
+	    message="Wohoo! You've got Blackjack! 😎"
+	    hasBlackJack=true
+	}
+	else{
+	    message="You're out of the game! 😭"
+	    isAlive=false
+	}
+    messageEl.textContent=message
 }
-else{
-    displayP.innerText="You're out of the game! 😭"
+function newCard() {
+    let card=6;
+    sum+=card;
+    renderGame()
+    
 }
+// CASH OUT
