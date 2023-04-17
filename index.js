20f4666c659c30a05622a29678494f0e54a472bf
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-04-17 22:52:14 +0000

    update

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
  // Import the functions you need from the SDKs you need
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+ import {
+    getFirestore,doc,getDoc,collection,updateDoc,addDoc,deleteDoc,getDocs
+} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
+
  // TODO: Add SDKs for Firebase products that you want to use
  // https://firebase.google.com/docs/web/setup#available-libraries
 
  // Your web app's Firebase configuration
  const firebaseConfig = {
-   apiKey: "AIzaSyASJLuDG5jhvDiaJ-GQ-keLR2Xhy2wNI-o",
-   authDomain: "mydemo-9802c.firebaseapp.com",
-   projectId: "mydemo-9802c",
-   storageBucket: "mydemo-9802c.appspot.com",
-   messagingSenderId: "636147442459",
-   appId: "1:636147442459:web:42dd3e3c25c62167fc29be"
+    apiKey: "AIzaSyASJLuDG5jhvDiaJ-GQ-keLR2Xhy2wNI-o",
+    authDomain: "mydemo-9802c.firebaseapp.com",
+    databaseURL: "https://mydemo-9802c-default-rtdb.firebaseio.com",
+    projectId: "mydemo-9802c",
+    storageBucket: "mydemo-9802c.appspot.com",
+    messagingSenderId: "636147442459",
+    appId: "1:636147442459:web:42dd3e3c25c62167fc29be"
+
  };
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig);
-
-import {
-    getFirestore,doc,getDoc,setDoc,collection,addDoc,updateDoc,deleteDoc,deleteField
-} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-firestore.js";
-
-const db=getFirestore()
+ const db=getFirestore(app)
 
 // References
 const first_name=document.getElementById('first_name');
 const last_name=document.getElementById('last_name');
-const emailBox=document.getElementById('Emailbox');
-const passwordBox=document.getElementById('Passwordbox');
+const email=document.getElementById('email');
+
 const phone=document.getElementById('phone');
 const website=document.getElementById('website');
 
@@ -37,88 +38,139 @@ const btnUpdate=document.getElementById('updBtn');
 const delBtn=document.getElementById('delBtn');
 const selBtn=document.getElementById('selBtn');
 
+// Get data from Firestore
+async function getDataFromFirestore() {
+    const querySnapshot = await getDocs(collection(db, "your_collection_name"));
+    const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    return data;
+  }
 
 
 
 async function AddDocument(){
-    var ref = doc(db, 'UserInfo', emailBox.value)
-    const docRef= await setDoc(
+    try{
+        var ref = collection(db, 'Persons')
+    const docRef= await addDoc(
         ref,{
             Firstname:first_name.value,
             Lastname:last_name.value,
-            Email:emailBox.value,
-            Password:passwordBox.value,
+            Email:email.value,
             Phone:phone.value,
             Website:website.value
 
         }
     )
     .then(()=>{
-        alert('Document successfully written!')
+        iziToast.success({
+            title: 'Success',
+            message: 'Document successfully added',
+        });
     })
-    .catch((error)=>{
-        alert('there was an error'+ error)
-    })
+    }
+    catch(error){
+        iziToast.error({
+            title: error,
+            message: 'Document unsuccessfully due to ' + error,
+        });
+    }
+    
 }
 
 // Get Document
 async function GetDocument(){
-    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || phone)
+    try{
+        var ref = doc(db, 'Persons',first_name.value || email.value || last_name.value || phone)
     const docSnap= await getDoc(ref)
     .then((docSnap)=>{
         if(docSnap .exists()){
             first_name.value=docSnap.data().Name
             last_name.value=docSnap.data().Lastname
-            emailBox.value=docSnap.data().Email
-            passwordBox.value=docSnap.data().Password
+            email.value=docSnap.data().Email
+        
             phone.value=docSnap.data().Phone
             website.value=docSnap.data().Website
 
+            iziToast.success({
+                title: 'Success',
+                message: 'Document successfully added',
+            });
+            
         }
+        
         else{
-            alert('No such document!')
+            iziToast.error({
+                title: error,
+                message: 'Document does not exist ',
+            });
         }
     })
-    .catch((error)=>{
-        alert(error)
-    })
+    }
+    catch(error){
+        iziToast.error({
+            title: error,
+            message: 'Document does not exist ',
+        });
+    }
 }
 // Update Fields
 async function UpdateFieldsInaDocument(){
-    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || last_name.value || phone.value || website)
+    try{
+        var ref = doc(db, 'Persons', email.value || first_name.value || last_name.value || phone.value || website)
     await updateDoc(ref,{
         Firstname:first_name.value,
+        theme: 'dark',
         Lastname:last_name.value,
-        Email:emailBox.value,
-        Password:passwordBox.value,
+        Email:email.value,
         Phone:phone.value,
         Website:website.value
     })
     .then(()=>{
-        alert('Document successfully written!')
+        iziToast.info({
+            title: 'Update',    
+            message: 'Document successfully updated ',
+        });
     })
-    .catch((error)=>{
-        alert('there was an error'+ error)
-    })
+    }
+    
+    catch(error){
+        iziToast.warning({
+            title: error,            
+            message: 'Updating unsucessful ',    
+        });
+    }
 }
 // Delete Document
 async function deleteDocument()
 {
-    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || phone)
+    try{
+        var ref = doc(db, 'Persons', email.value || first_name.value || last_name.value  || phone)
+    const docSnap= await getDoc(ref)
     if(!docSnap.exists()){
-        alert('Document does not exist')
+        iziToast.warning({
+            title: error,
+            message: 'Document does not exist ',
+            
+        });
         return
     }
     await deleteDoc(ref)
     .then(()=>{
-        alert('Document deleted successfully!')
-    })
-    .catch((error)=>{
-        alert('there was an error'+ error)
+        iziToast.success({
+            title: 'Success',
+            message: 'Document deleted successfully ',
+            
+        });
     })
     
+    }
+    catch(error){
+        iziToast.error({
+            title: error,
+            message: 'Document unsuccessfully due to ' + error,
+            
+        });
+    }
 }
-
 
 
 // Assign Events to Buttons
@@ -126,3 +178,18 @@ submitBtn.addEventListener('click', AddDocument)
 selBtn.addEventListener('click', GetDocument)
 btnUpdate.addEventListener('click', UpdateFieldsInaDocument)
 delBtn.addEventListener('click', deleteDocument)
+// Toast notifications
+iziToast.settings({
+    theme: "dark",
+    timeout: 5000,
+    overlay: true,
+    position:'center',
+    progressBarColor: 'rgb(0, 255, 184)',
+    resetOnHover: true,
+    transitionIn: 'bounceInDown',
+    transitionOut: 'fadeOutUp',
+    pauseOnHover: true,
+    overlayColor: 'rgba(0, 0, 0, 0.6)',
+    balloon: true,
+    });
+
