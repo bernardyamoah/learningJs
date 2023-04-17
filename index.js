bf317dda8c0c98250ae202c00a700c038e541b55
Bernard Yamoah <bernardyamoah2016@gmail.com>
2023-04-17 15:57:53 +0000

    🎨 style(index.html): refactor form layout and input fields ✨ feat(index.html): add new input fields for website and last name 🚀 chore(index.html): remove unused divs and classes The form layout has been refactored to use a grid system, which improves readability and makes it easier to add new input fields. New input fields for website and last name have been added to the form. Unused divs and classes have been removed to simplify the HTML code.

--- index.js
+++ index.js
@@ -23,27 +23,34 @@ import {
 const db=getFirestore()
 
 // References
-const nameBox=document.getElementById('Namebox');
+const first_name=document.getElementById('first_name');
+const last_name=document.getElementById('last_name');
 const emailBox=document.getElementById('Emailbox');
 const passwordBox=document.getElementById('Passwordbox');
-const phoneBox=document.getElementById('Phonebox');
+const phone=document.getElementById('phone');
+const website=document.getElementById('website');
 
 
 // Buttons
 const submitBtn=document.getElementById('submitBtn');
 const btnUpdate=document.getElementById('updBtn');
+const delBtn=document.getElementById('delBtn');
+const selBtn=document.getElementById('selBtn');
 
 
 
 
 async function AddDocument(){
-    var ref = collection (db, 'UserInfo')
-    const docRef= await addDoc(
+    var ref = doc(db, 'UserInfo', emailBox.value)
+    const docRef= await setDoc(
         ref,{
-            Name:nameBox.value,
+            Firstname:first_name.value,
+            Lastname:last_name.value,
             Email:emailBox.value,
             Password:passwordBox.value,
-            Phone:phoneBox.value
+            Phone:phone.value,
+            Website:website.value
+
         }
     )
     .then(()=>{
@@ -53,3 +60,69 @@ async function AddDocument(){
         alert('there was an error'+ error)
     })
 }
+
+// Get Document
+async function GetDocument(){
+    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || phone)
+    const docSnap= await getDoc(ref)
+    .then((docSnap)=>{
+        if(docSnap .exists()){
+            first_name.value=docSnap.data().Name
+            last_name.value=docSnap.data().Lastname
+            emailBox.value=docSnap.data().Email
+            passwordBox.value=docSnap.data().Password
+            phone.value=docSnap.data().Phone
+            website.value=docSnap.data().Website
+
+        }
+        else{
+            alert('No such document!')
+        }
+    })
+    .catch((error)=>{
+        alert(error)
+    })
+}
+// Update Fields
+async function UpdateFieldsInaDocument(){
+    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || last_name.value || phone.value || website)
+    await updateDoc(ref,{
+        Firstname:first_name.value,
+        Lastname:last_name.value,
+        Email:emailBox.value,
+        Password:passwordBox.value,
+        Phone:phone.value,
+        Website:website.value
+    })
+    .then(()=>{
+        alert('Document successfully written!')
+    })
+    .catch((error)=>{
+        alert('there was an error'+ error)
+    })
+}
+// Delete Document
+async function deleteDocument()
+{
+    var ref = doc(db, 'UserInfo', emailBox.value || first_name.value || phone)
+    if(!docSnap.exists()){
+        alert('Document does not exist')
+        return
+    }
+    await deleteDoc(ref)
+    .then(()=>{
+        alert('Document deleted successfully!')
+    })
+    .catch((error)=>{
+        alert('there was an error'+ error)
+    })
+    
+}
+
+
+
+// Assign Events to Buttons
+submitBtn.addEventListener('click', AddDocument)
+selBtn.addEventListener('click', GetDocument)
+btnUpdate.addEventListener('click', UpdateFieldsInaDocument)
+delBtn.addEventListener('click', deleteDocument)
